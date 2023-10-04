d6da367acb0f564904313326c7f008ebf3e410d1
ahmedamerworks <ahmedamerworks@gmail.com>
2023-10-04 17:11:01 -0400

    adjusted class and id names for html elements to simplify css code

--- index.js
+++
@@ -1,8 +0,0 @@
-function formRender() {
-  const form = document.getElementsByClassName("form-container");
-  if (form.style.display === "none") {
-    form.style.display = "flex";
-  }
-}
-
-export default formRender;
